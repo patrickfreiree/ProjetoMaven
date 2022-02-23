92d0d94e6746d4413c21264e9c236109d43efca6
oatrickfreiree <patrickfaed@gmail.com>
2022-02-23 01:40:56 -0300

    Adicionando calculo subtracao e teste

--- src/test/java/CalculadoraTeste.java
+++ src/test/java/CalculadoraTeste.java
@@ -7,6 +7,11 @@ public class CalculadoraTeste {
 	public void testSoma(){
 		Calculadora calculadora = new Calculadora(2,3);
 		assertEquals(5.0, calculadora.soma(), 0.001);
-
 	}
-}
+
+//	@Test
+//	public void testeMenos(){
+//		Calculadora calculadora = new Calculadora(4,4);
+//		assertEquals(0.0, calculadora.soma(), 5);
+//	}
+		}
